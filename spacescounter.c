55df3b4ae69ac3b3dedb1b792597dcf1fbc1dfa6
egorlihachev <>
2025-12-28 15:47:50 +0500

    еще немного задачки

---
+++ spacescounter.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  int c, spaces, tabs;
+
+  while ((c = getchar()) != EOF)
+    if (c == ' ')
+      spaces++;
+  if (c = '\t')
+    tabs++;
+
+  printf("%d %d\n", spaces, tabs);
+
+  return 0;
+}
